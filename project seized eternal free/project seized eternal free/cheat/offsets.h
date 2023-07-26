45969a551c53af7a4669bf3d49975d663184e800
seized1338 <130160018+seized1338@users.noreply.github.com>
2023-07-26 23:59:34 +0200

    Update offsets.h

--- project seized eternal free/project seized eternal free/cheat/offsets.h
+++ project seized eternal free/project seized eternal free/cheat/offsets.h
@@ -2,24 +2,24 @@
 
 enum offsets : ptrdiff_t {
 	PersistentLevel = 0x30, // almost never changes
-	Uworld = 0xEA84A78, // updated
+	Uworld = 0xEE2F8E8, // updated
 	OwningGameInstance = 0x1b8, // updated
-	PlayerCameraManager = 0x340, // updated
+	PlayerCameraManager = 0x348, // updated
 	LocalPlayers = 0x38, // almost never change
-	AcknowledgedPawn = 0x330, //updated
-	PlayerState = 0x2A8, // updated
+	AcknowledgedPawn = 0x338, //updated
+	PlayerState = 0x2B0, // updated
 	PlayerController = 0x30, // updated
-	PlayerArray = 0x2a0, // updated
-	RootComponent = 0x190, // updated
+	PlayerArray = 0x2A8, // updated
+	RootComponent = 0x198, // updated
 	RelativeLocation = 0x128, // updated
 	AActor = 0x98, // almost never changes
 	CurrentWeapon = 0x948, // updated
 	WeaponData = 0x438, // updated
-	Mesh = 0x310, // updated
+	Mesh = 0x318, // updated
 	Levels = 0x170, // updated
 	Tier = 0x73, // updated
-	TeamID = 0x10C8, // updated
+	TeamID = 0x10D8, // updated
 	GameState = 0x158, // updated
-	ReviveFromDBNOTime = 0x4698, // updated
-	PawnPrivate = 0x300, // updated
+	ReviveFromDBNOTime = 0x46b8, // updated
+	PawnPrivate = 0x308, // updated
 };
